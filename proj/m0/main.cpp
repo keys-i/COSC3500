5467975eae85200ded346db6e733370ad43cdce3
Keys <70819367+keys-i@users.noreply.github.com>
2026-08-05 06:06:56 +1000

    feat: add the m0 baseline
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

---
+++ proj/m0/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main() {
+    std::puts("My topic is a config-based, multi-behaviour predator–prey "
+              "simulation, and "
+              "it’s about using configurable behaviours with a custom "
+              "rendering module");
+}
