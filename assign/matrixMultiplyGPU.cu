60510efc830ed926f664fcf17a09e5377113e187
Keys <70819367+keys-i@users.noreply.github.com>
2026-09-14 23:00:45 +1000

    made a GPU baseline for CUDA version
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- assign/matrixMultiplyGPU.cu
+++ assign/matrixMultiplyGPU.cu
@@ -1,3 +1,6 @@
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
 #include <matrixMultiplyGPU.cuh>
 #define STUDENTID 49088276 //DO NOT REMOVE
 /**
@@ -12,11 +15,28 @@
 * @return : your student ID
 *  	 	   			     	 		 			 	      
 * */
+
 __host__ int matrixMultiply_GPU(int N, const floatTypeCUDA* A, const floatTypeCUDA* B, floatTypeCUDA* C, int* flags, int flagCount){  	 	   			     	 		 			 	      
     //Your code must be able to deal with N=0 scenario without crashing.
     if (N<=0) return STUDENTID;
 
     //WRITE YOUR CODE HERE
+
+    const dim3 block(16, 16);
+    const unsigned tiles = 1u + (unsigned(N) - 1u) / 16u;
+    const dim3 grid(tiles, tiles);
+
+    matrixMultiplyKernel_GPU<<grid, block>>(N, A, B C, 0, 0, 0);
+
+    cudaError_t err = cudaGetLastError();
+    if (err == cudaSuccess) 
+        err = cudaDeviceSynchronize();
+
+    if (err != cudaSuccess) {
+        std::fprintf(stderr, "CUDA multiplication failed: %s\n",
+                     cudaGetErrorString(err));
+        std::abort();
+    }
 
     return STUDENTID;
 
@@ -24,5 +44,25 @@ __host__ int matrixMultiply_GPU(int N, const floatTypeCUDA* A, const floatTypeCU
 
 //The kernel (device code) parameters have been setup almost the same as the host code, except the flags are passed in individually rather than as a pointer. This is done just so you don't have to copy the parameters to GPU memory first, you'll be able to pass in up to 3 on the function call.  	 	   			     	 		 			 	      
 __global__ void matrixMultiplyKernel_GPU(int N, const floatTypeCUDA* A, const floatTypeCUDA* B, floatTypeCUDA* C, int flag0, int flag1, int flag2){  	 	   			     	 		 			 	      
+    const std::size_t row = size_t(blockIdx.x) * blockDim.x + threadIdx.x;
+    const std::size_t col = size_t(blockIdx.y) * blockDim.y + threadIdx.y;
+    if (row >= size_t(N) || col >= size_t(N))
+        return;
 
+    float real = 0.0f, imag = 0.0f;
+
+    for (int k = 0; k <  N; ++k) {
+        const floatTypeCUDA a = A[row + size_t(k) * N];
+        const floatTypeCUDA b = B[k + col * N];
+
+        real += a.x * b.x - a.y * b.y
+        imag += a.x * b.y + a.y * b.x;
+    }
+
+    floatTypeCUDA result;
+
+    result.x = real;
+    result.y = imag;
+
+    C[row + col * N] = result;
 }
